f5df98fe6b3f71c54a9507e624aa417d2335b856
user1 <user1@KH_C>
2018-01-15 08:55:37 +0900

    180112 연산자, 조건문, 반복문 업데이트

--- Chapter02/src/chap02/ex09/continueEx/Ex01.java
+++ Chapter02/src/chap02/ex09/continueEx/Ex01.java
@@ -5,13 +5,19 @@ public class Ex01 {
 	// 1부터 10까지 3의 배수가 아닌 수 출력
 	public static void main(String[] args) {
 		
-		for (int i = 1; i <= 10; i++) {
-			if (i % 3 == 0) {
-				continue;
-			}
-			System.out.println(i);
-		}
+//		for (int i = 1; i <= 10; i++) {
+//			if (i % 3 == 0) {
+//				continue;
+//			}
+//			System.out.println(i);
+//		}
 
+		for (int i = 0; i < 5; i++) {
+			for (int j = 0; j < 5; j++) {
+				if (j == 3) break;
+				System.out.println(i + " " + j);
+			}
+		}
 	}
 
 }
